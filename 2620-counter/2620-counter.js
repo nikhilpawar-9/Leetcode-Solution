d6ae73d13438d2c08f344a2e8b0c994f1972b9c3
Nikhil Pawar <nikhil0909pawar@gmail.com>
2026-08-31 23:59:38 +0530

    Time: 41 ms (66.31%), Space: 53.2 MB (71.74%) - LeetHub

---
+++ 2620-counter/2620-counter.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {Function} counter
+ */
+var createCounter = function(n) {
+    let count = n;
+    return function() {
+        let curr = count;
+        count++;
+        return curr;
+    };
+};
+
+/** 
+ * const counter = createCounter(10)
+ * counter() // 10
+ * counter() // 11
+ * counter() // 12
+ */
