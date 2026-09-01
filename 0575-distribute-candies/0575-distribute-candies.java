efd7c153edaef930ac437bcc8c805c82a9a5312d
Nikhil Pawar <nikhil0909pawar@gmail.com>
2026-09-01 20:48:52 +0530

    Time: 30 ms (92.04%), Space: 48.3 MB (92.6%) - LeetHub

---
+++ 0575-distribute-candies/0575-distribute-candies.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int distributeCandies(int[] nums) {
+        int n = nums.length / 2;
+        Set<Integer> set = new HashSet<>();
+        for(int num : nums) set.add(num);
+        if(set.size() > n) return n;
+        else return set.size();
+    }
+}
